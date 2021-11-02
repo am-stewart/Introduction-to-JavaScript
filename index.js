a320fd4eacdaa53d3199ab262aa049cc462c24ee
Allison Stewart <amstewart@yahoo.com>
2021-11-02 18:19:11 -0400

    completed task 4

--- index.js
+++ index.js
@@ -158,10 +158,28 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
-
-function game(user, computer){
-  /*add your code here*/
+let computer = Math.random();
+if (computer <= 0.33) {
+  computer = 'rock';
+} else if (computer <= 0.66) {
+  computer = 'paper';
+} else if (computer > 0.66) {
+  computer = 'scissors'
 }
+function game(user, computer){
+  if (user === computer) {
+    return `it's a tie`;
+  } else if (user === 'rock' && computer === 'scissors') {
+    return 'you win!';
+  } else if (user === 'paper' && computer === 'rock') {
+    return 'you win!';
+  } else if (user === 'scissors' && computer === 'paper') {
+    return 'you win!';
+  } else {
+    return 'you lose!';
+  }
+}
+console.log ('task 4:', game('paper','rock'));
 
 
 
