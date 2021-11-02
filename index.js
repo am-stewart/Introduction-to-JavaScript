93f630c976ebfceac4e02921a681196283541aa5
Allison Stewart <amstewart@yahoo.com>
2021-11-02 18:45:52 -0400

    completed all tasks 1-7

--- index.js
+++ index.js
@@ -194,9 +194,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
+console.log ('task 5a:', miles (3));
 
 
 
@@ -208,9 +209,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
+console.log ('task 5b:', feet(100));
 
 
 
@@ -224,9 +226,11 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+  for (let i = number; i > 0; i--)
+      return `${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around ${number - 1} bottles of soda on the wall`;
 }
+console.log('task 6:', annoyingSong(4));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -244,9 +248,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+if (number >= 90) {
+  return 'you got an A';
+} else if (number < 90 && number >= 80) {
+  return 'you got a B';
+} else if (number < 80 && number >= 70) {
+  return 'you got a C';
+} else if (number < 70 && number >= 60) {
+  return 'you got a D';
+} else if (number < 60) {
+  return 'you got an F';
 }
+}
+console.log ('task 7:', grade (86));
 
 
 
